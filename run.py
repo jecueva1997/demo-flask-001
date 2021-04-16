297acc93d052f6d9c561dca0da24811470a6126c
jecueva1997 <reatiguijeferson@gmail.com>
2021-04-16 12:43:51 -0500

    Listado

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
 @app.route('/listado')
 def lista():
-    return  "<h2>Carreras de la UTPL</h2> <ul> <li type='circle'>Esto es un tipo de punto.</li> <li type='square'>Este es otro.</li> <li type='disc'>Y este es otro diferente.</li></ul>"
+    return  "<h2>Carreras de la UTPL de la Área Técnica</h2> <ul> <li type='circle'>Arquitectura</li> <li type='circle'>Arte y Diseño</li> <li type='circle'>Electrónica y Telecomunicaciones</li> <li type='circle'>Geología y Minas</li> <li type='circle'>Ingeniería Civil</li> <li type='circle'>Sistemas Informátiocos</li></ul>"
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
